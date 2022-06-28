ef75afd8b298a9a6a35f118e234fef7a1a57e8d7
quinn305 <violacat99@gmail.com>
2022-06-28 12:01:08 -0500

    Add files via upload

---
+++ rcParameters.py
@@ -0,0 +1,24 @@
+plt.rcParams['axes.linewidth'] = 1.5
+plt.rcParams['axes.titlesize'] = 24
+plt.rcParams['axes.labelsize'] = 26
+plt.rcParams['figure.figsize'] = (8, 6)
+plt.rcParams['xtick.labelsize'] = 22
+plt.rcParams['ytick.labelsize'] = 22
+plt.rcParams['legend.framealpha'] = 0.
+plt.rcParams['legend.markerscale'] = 2.
+plt.rcParams['legend.fontsize'] = 'xx-large'
+plt.rcParams['figure.subplot.wspace'] = 0.02
+plt.rcParams['figure.subplot.hspace'] = 0.02
+plt.rcParams['axes.labelpad'] = 5.
+
+plt.rcParams['xtick.top'] = plt.rcParams['ytick.right'] = True
+plt.rcParams['xtick.minor.visible'] = plt.rcParams['ytick.minor.visible'] = True
+plt.rcParams['xtick.direction'] = plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams['xtick.major.size'] = plt.rcParams['ytick.major.size'] = 8 
+plt.rcParams['xtick.minor.size'] = plt.rcParams['ytick.minor.size'] = 5
+plt.rcParams['xtick.major.width'] = plt.rcParams['ytick.major.width'] = 1; 
+
+plt.rcParams['savefig.bbox']='tight'
+plt.rcParams['savefig.transparent']=True
+
+#matplotlib.rcdefaults()
